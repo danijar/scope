2550a2703671d1bf1d7c070bba79f46e32bd40cb
Danijar Hafner <mail@danijar.com>
2024-10-02 01:35:13 +0000

    Add search icons

--- viewer/client/src/Selector.vue
+++ viewer/client/src/Selector.vue
@@ -52,8 +52,11 @@ function unselect(item) {
 <template>
 <div class="selector">
   <h2 v-if="props.title.length">{{ props.title }}</h2>
-  <input v-model="state.pattern" placeholder="Search" />
-  <div>
+  <label class="search">
+    <span class="icon">search</span>
+    <input v-model="state.pattern" placeholder="Search" />
+  </label>
+  <div class="list">
     <ul v-if="selectedDisplay.length">
       <li v-for="item in selectedDisplay" @click="unselect(item)" class="selected"><span>{{ item }}</span></li>
     </ul>
@@ -67,12 +70,14 @@ function unselect(item) {
 <style scoped>
 .selector { display: flex; flex-direction: column; }
 
-.selector > h2 { flex: 0 1 content; margin: 0 .2rem .4rem; font-size: 1.3rem; font-weight: 500; color: #333; }
-.selector > input { flex: 0 0 content; margin: 0; padding: .3rem .2rem; font-family: monospace; border: none; border-radius: .2rem .2rem 0 0; font-size: 0.955rem; color: #777; }
-.selector > input::placeholder { color: #777; }
-.selector > input:focus { outline: none; }
-.selector > div { flex: 1 1 content; overflow: scroll; /* background: #fff; */ }
+h2 { flex: 0 1 content; margin: 0 .2rem .4rem; font-size: 1.3rem; font-weight: 500; color: #333; }
 
+.search .icon { font-size: 1.4rem; color: #aaa; margin: 0 .1rem .1rem 0; }
+.search input { flex: 0 0 content; margin: 0; padding: .2rem; font-family: monospace; border: none; border-radius: .2rem .2rem 0 0; color: #777; }
+.search input::placeholder { color: #777; }
+.search input:focus { outline: none; }
+
+.list { flex: 1 1 content; overflow: scroll; }
 ul { list-style: none; padding: 0; margin: .5rem 0; }
 ul + ul { padding-top: 0; }
 
